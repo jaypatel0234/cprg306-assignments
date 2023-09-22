f5b2122a702f4fc24a5d63db1c68477fe23187c5
jaypatel0234 <120231323+jaypatel0234@users.noreply.github.com>
2023-09-22 10:03:48 -0600

    Update page.js

---
+++ app/Week 2/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import Link from "next/link";
+
+export default function Home() {
+  return<main>Hello World
+    
+    <p><Link href="/Week2">Week 2</Link></p>
+    </main>;
+}
