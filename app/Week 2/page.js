e2551b37d9435f246d8e3ca3ec4dd6fc2230973e
jaypatel0234 <120231323+jaypatel0234@users.noreply.github.com>
2023-09-22 10:06:35 -0600

    Delete page.js

--- app/Week 2/page.js
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-import Link from "next/link";
-
-export default function Home() {
-  return<main>Hello World
-    
-    <p><Link href="/Week2">Week 2</Link></p>
-    </main>;
-}
