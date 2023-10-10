c4b46caf0d452836ae903be35740ea56277848ac
jaypatel0234 <120231323+jaypatel0234@users.noreply.github.com>
2023-10-09 21:25:12 -0600

    Week 4

--- app/page.js
+++ app/page.js
@@ -18,6 +18,7 @@ const Page = () => {
       <StudentInfo />
       <Link href="/week2">Go to Week 2</Link>
       <Link href="/week3">Go to Week 3</Link>
+      <Link href="/week4">Go to Week 4</Link>
     </div>
   );
 };
---
+++ app/week4/page.js
@@ -0,0 +1,15 @@
+"use client";
+import NewItem from './new-item';
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Week 4 Assignment</h1>
+      <NewItem />
+    </div>
+  );
+};
+
+export default Page;
+
+
